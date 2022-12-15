a8570651b82ae65ba20b8f46577a24f6ecae8fa8
gaurav-123r <96628896+gaurav-123r@users.noreply.github.com>
2022-12-15 16:48:14 +0530

    hi

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 def count_batteries_by_usage(cycles):
+  l=0
+  m=0
+  h=0
+  for i in cycles:
+    if i<310:
+      l=l+1
+    if i>=310 and i<=210:
+      m=m+1
+    if i>929:
+      h=h+1
+
+
   return {
     "lowCount": 0,
     "mediumCount": 0,
